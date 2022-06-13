dbe94531d4cac6a3591d093e2b2fa6364f178d81
Eric Nantes <eric.nantes@lumis.com.br>
2022-06-13 17:26:38 -0400

    adaptando evolo para o layout

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@
 const { resolve } = require('path')
 import handlebars from 'vite-plugin-handlebars';
 
-import { pageData } from './src/partials/partialsConfig';
-
 const config = {
   root: 'src',
   build: {
@@ -13,8 +11,8 @@ const config = {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        teste: resolve(__dirname, 'src/teste.html'),
-        api: resolve(__dirname, 'src/api.html'),
+        'terms-conditions': resolve(__dirname, 'src/terms-conditions.html'),
+        'privacy-policy': resolve(__dirname, 'src/privacy-policy.html'),
       },
       output: {
         entryFileNames: `assets/[name].js`,
@@ -23,12 +21,29 @@ const config = {
       },
     },
   },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
   plugins: [handlebars({
     context(pagePath) {
       return pageData[pagePath];
     },
     partialDirectory: resolve(__dirname, 'src/partials'),
   })]
+}
+
+const pageData = {
+  '/index.html': {
+    title: 'Página Inicial',
+  },
+  '/terms-conditions.html': {
+    title: 'Termos e condições',
+  },
+  '/privacy-policy.html': {
+    title: 'Política e privacidade',
+  }
 }
 
 export default config
